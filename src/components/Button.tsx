3494abcbe0f1256dbe055a661268fbddf040540f
MOID UDDIN <moiduddin990@gmail.com>
2024-11-05 04:31:05 +0500

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div className="w-[288px] h-[58px] rounded-[10px] bg-[#A29875] flex justify-center items-center">
+      <h1 className=" text-[30px] font-medium  text-[#FFFFFF] ">
+        Explore Now
+      </h1>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="h-[134px] bg-[#A29875]">
+      <h1 className="text-[75px] px-6 py-3 text-[#FFFFFF]">MANZZARI</h1>
+    </div>
+  );
+}
+
+export default Header;
